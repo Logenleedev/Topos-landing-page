9b777372a38bf7e916411494cd3b85a98bdaa929
rieder <riederlee@gmail.com>
2020-04-17 20:33:14 +0800

    push the first section

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ var express = require("express");
     controls = require('./control/control.js'),
     fs = require('fs');
 
+    global.__basedir = __dirname;
 //Set up the port
-let port = process.env.PORT || 3000;
+let port = process.env.PORT || 8000;
 
 //Host static asset
 app.use(express.static('Semantic-UI-CSS-master'));
@@ -16,33 +17,20 @@ app.use(express.static(path.join(__dirname,'src')));
 app.use(express.static(path.join(__dirname,'Assest')));
 
 
-
 // bodyparser middleware
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended:false}));
 
-//Set up controller
-
-app.use('/',controls);
-
 //HOME GET
 app.get('/',(req,res)=>{
-    res.writeHead(200, {
-        'Content-Type': 'text/html'
-    });
-    fs.readFile('./src/views/main.html', null, function (error, data) {
-        if (error) {
-            res.writeHead(404);
-            res.write('Whoops! File not found!');
-        } else {
-            res.write(data);
-        }
-        res.end();
-    });
+    res.sendFile('./src/views/main.html',  { root: __dirname });
+})
+
+app.get('/about-us',(req,res)=> {
+    res.sendFile('./src/views/Team.html',  { root: __dirname });
 })
 
 
-
 app.listen(port, function(){
-    console.log('Server Started on Port 3000')
+    console.log('Server Started on Port 8000')
 });
